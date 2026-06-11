b91b2f9e00f3768c7e2cbab8585b66bac4c1a52d
Santiago Leal <santiagoald0203@gmail.com>
2026-06-11 11:30:58 -0400

    agregando modos

--- js/endScreen.js
+++ js/endScreen.js
@@ -1,3 +1,4 @@
+
 function mostrarPantallaFinal() {
   const overlay = document.getElementById('end-screen-overlay');
   const endScreen = document.getElementById('end-screen');
@@ -10,3 +11,21 @@ function mostrarPantallaFinal() {
   }
 
   let html = '';
+
+  if (gameState.mode === 'pvp') {
+    const p1 = gameState.players[0];
+    const p2 = gameState.players[1];
+    
+    let titulo = '';
+    let classP1 = 'pvp-card';
+    let classP2 = 'pvp-card';
+    
+    if (p1.score > p2.score) {
+      titulo = `🎉 ¡${p1.name} gana!`;
+      classP1 += ' winner';
+    } else if (p2.score > p1.score) {
+      titulo = `🎉 ¡${p2.name} gana!`;
+      classP2 += ' winner';
+    } else {
+      titulo = `🤝 ¡Es un Empate!`;
+    }
